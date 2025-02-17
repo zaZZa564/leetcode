d48fd8753bee88180de38193a34433a21ba73183
zaZZa564 <anus_bobra564@mail.ru>
2025-02-17 15:29:41 +0300

    solved binary search

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
+int binarySearch(vector<int>, int);
+
 int main() {
-  
+  vector<int> testArr;
   return 0;
+}
+
+int binarySearch(vector<int> nums, int target) {
+  int low = 0, high = nums.size() - 1, mid, guess;
+
+  while(low <= high) {
+    mid = floor((low + high) / 2);
+    guess = nums[mid];
+
+    if(guess == target) {
+      return mid;
+    }
+    else if(guess > target) {
+      high = mid - 1;
+    }
+    else {
+      low = mid + 1;
+    }
+  }
+  return -1;
 }
